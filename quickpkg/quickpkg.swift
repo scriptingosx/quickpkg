4b13e319eb5efc3e6678f858b9a0ff8a784c9de1
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2023-08-08 17:20:02 +0200

    starting out...

--- quickpkg/quickpkg.swift
+++ quickpkg/quickpkg.swift
@@ -17,6 +17,8 @@ struct QuickPkg: ParsableCommand {
     version: Constants.version
   )
 
+  // MARK: Arguments and Options
+
   @Argument(help: ArgumentHelp(
     "Path to the item to build a installer pkg from.",
     valueName: "installer-item"))
@@ -25,19 +27,33 @@ struct QuickPkg: ParsableCommand {
   struct ScriptsOptions: ParsableArguments {
     @Option(name: .customLong("scripts"),
             help: "Path to a folder with scripts.")
-    var scriptsFolder: String
+    var scriptsFolder: String?
 
     @Option(name: .customLong("preinstall"),
             help: "Path to the preinstall script.")
-    var preinstall: String
+    var preinstall: String?
 
     @Option(name: .customLong("postinstall"),
             help: "Path to the postinstall script.")
-    var postinstall: String
+    var postinstall: String?
   }
 
   @OptionGroup(title: "Installation Scripts")
   var scriptsOptions: ScriptsOptions
+
+  struct SignOptions: ParsableArguments {
+    @Option(name: .long, help: "Adds a digital signature to the resulting package.")
+    var sign: String?
+
+    @Option(name: .long, help: "Specify a specific keychain to search for the signing identity.")
+    var keychain: String?
+
+    @Option(name: .long, help: "Specify an intermediate certificate to be embedded in the package.")
+    var cert: String?
+  }
+
+  @OptionGroup(title: "Signing")
+  var signOptions: SignOptions
 
   @Option(name: .long, help: "Install-location for the resulting pkg.")
   var installLocation: String = "/Applications"
@@ -46,7 +62,7 @@ struct QuickPkg: ParsableCommand {
 Path where the package file will be created.
 You can use '{name}', '{version}' and '{identifier}' as placeholders. If this is a directory, then the package will be created with the default filename {name}-{version}.pkg
 """)
-  var output: String
+  var output: String?
 
   enum Ownership: String, ExpressibleByArgument {
     case recommended, preserve, preserveOther = "preserve-other"
@@ -61,24 +77,80 @@ You can use '{name}', '{version}' and '{identifier}' as placeholders. If this is
   @Flag(inversion: .prefixedNo, help: "Sets BundleIsRelocatable in the PackageInfo to true.")
   var relocatable = false
 
-  struct SignOptions: ParsableArguments {
-    @Option(name: .long, help: "Adds a digital signature to the resulting package.")
-    var sign: String
-
-    @Option(name: .long, help: "Specify a specific keychain to search for the signing identity.")
-    var keychain: String
-
-    @Option(name: .long, help: "Specify an intermediate certificate to be embedded in the package.")
-    var cert: String
-  }
-
-  @OptionGroup(title: "Signing")
-  var signOptions: SignOptions
-
   @Flag(name: .shortAndLong, help: "Controls amount of logging output (max -vvv).")
   var verbosity: Int
 
-  func run() {
-    print("installer-item: \(itemPath), verbosity: \(verbosity)")
+  // MARK: Properties
+
+  lazy var tempDir: URL = {
+    var randomNumber = Int.random(in:1000000...9999999)
+    var tempDir = FileManager.default.temporaryDirectory
+      .appendingPathComponent(
+        "quickpkg.\(randomNumber)",
+        isDirectory: true
+      )
+    while FileManager.default.fileExists(atPath: tempDir.path) {
+      randomNumber += 1
+      tempDir = FileManager.default.temporaryDirectory
+        .appendingPathComponent(
+          "quickpkg.\(randomNumber)",
+          isDirectory: true
+        )
+    }
+    do {
+      try FileManager.default.createDirectory(at: tempDir, withIntermediateDirectories: false)
+      return tempDir
+    } catch {
+      print("Could not create temporary directory at \(tempDir.path)!")
+      cleanupAndExit(code: 1)
+    }
+  }()
+
+  lazy var itemURL: URL = URL(filePath: itemPath)
+
+  lazy var itemBasename = itemURL.deletingPathExtension().lastPathComponent
+  lazy var itemExtension = itemURL.pathExtension
+
+
+  // MARK: functions
+
+  func cleanupAndExit(code: Int32 = 0) -> Never {
+    if code != 0 {
+      Self.exit(withError: ExitCode(code))
+    }
+    Self.exit()
   }
+
+  // MARK: main
+  mutating func run() {
+    // remove trailing '/'
+    if itemPath.hasSuffix("/") {
+      itemPath = String(itemPath.dropLast())
+    }
+
+    // expand homedir tilde
+    itemPath = NSString(string: itemPath).expandingTildeInPath
+
+    if !Constants.supportedExtensions.contains(itemExtension) {
+      print("\(itemExtension) is not a supported file type!")
+      cleanupAndExit(code: 1)
+    }
+
+    // extract app path from archive
+
+    // copy or move app
+
+    // get metadata from app
+
+    // create the component plist
+
+    // prepare pkgbuild command
+
+    // prepare scripts folder
+
+    // run pkgbuild command
+
+    // cleanup
+
+   }
 }
