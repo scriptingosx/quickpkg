0fdb475ffced1a96642b3dddfe323b8ca73dee3e
Armin Briegel <1933192+scriptingosx@users.noreply.github.com>
2026-02-15 14:30:45 +0100

    reformatted

--- Sources/quickpkg/InputType.swift
+++ Sources/quickpkg/InputType.swift
@@ -2,19 +2,19 @@ import ArgumentParser
 import Foundation
 
 enum InputType: String, CaseIterable {
-    case app
-    case dmg
-    case zip
-    case xip
-
-    static func from(path: String) -> InputType? {
-        let ext = URL(filePath: path).pathExtension.lowercased()
-        return InputType(rawValue: ext)
-    }
+  case app
+  case dmg
+  case zip
+  case xip
+  
+  static func from(path: String) -> InputType? {
+    let ext = URL(filePath: path).pathExtension.lowercased()
+    return InputType(rawValue: ext)
+  }
 }
 
 enum Ownership: String, ExpressibleByArgument, CaseIterable {
-    case recommended
-    case preserve
-    case preserveOther = "preserve-other"
+  case recommended
+  case preserve
+  case preserveOther = "preserve-other"
 }
